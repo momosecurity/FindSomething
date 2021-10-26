684e46b6b489cba9788c2f0fa50de5e9949c0a93
ResidualLaugh <1015796131@qq.com>
2021-10-26 10:31:55 +0800

    修改了展示数据的输出方式 (#8)
    ver: 2.0.4  更新数据的展示方式

--- popup.js
+++ popup.js
@@ -1,22 +1,19 @@
 // @Date    : 2020-09-12 16:26:48
 // @Author  : residuallaugh
 const bg = chrome.extension.getBackgroundPage();
-var result;
-var current;
 var key = ["ip","ip_port","domain","path","url","static","sfz","mobile","mail","jwt","algorithm"]
 chrome.tabs.getSelected(null, function (tab) {
-        current=tab.url;
-        result = bg.result(current);
-        console.log(result)
+        var current=tab.url;
+        var result_data = bg.result(current);
         for (var k in key){
-            if (result[key[k]]!=null && result[key[k]].length != 0){
-                console.log(result[key[k]])
+            if (result_data[key[k]]!=null && result_data[key[k]].length != 0){
+                // console.log(result_data[key[k]])
                 let p="";
-                for(var i in result[key[k]]){
-                    p = p + result[key[k]][i] +'\n'
+                for(var i in result_data[key[k]]){
+                    p = p + result_data[key[k]][i] +'\n'
                 }
-                document.getElementById(key[k]).innerText=p;
+                $("p#"+key[k]).css("white-space","pre");
+                $("p#"+key[k]).text(p);
             }
         }
 });
-
