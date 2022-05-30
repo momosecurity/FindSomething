cf3d5744d1e5ebabc5d75b1b88d4567d9194987c
ResidualLaugh <1015796131@qq.com>
2022-05-30 14:03:42 +0800

    ver:2.0.7 (#15)
    ### 添加
    - 新增了通过关键词提取部分敏感数据，使用了nuclei的规则，感谢开源。https://github.com/projectdiscovery/nuclei-templates/blob/master/exposures/tokens/generic/credentials-disclosure.yaml
    - 新增了复制功能，一键复制该区域结果
    - 新增了非标准开头的路径提取，这将会存在一些误报。如 api/path
    ### 变更
    - 将提取部分移到背景页中，避免前端加载页面时出现阻塞
    ### 修复
    - 尝试修复了部分数据提取不到的问题

--- content.js
+++ content.js
@@ -18,7 +18,7 @@
 
     var source_href = source.match(/href=['"].*?['"]/g);
     var source_src = source.match(/src=['"].*?['"]/g);
-    var script_src = source.match(/<script [^><]+src=\".*?\"/g);
+    var script_src = source.match(/<script [^><]*?src=['"].*?['"]/g);
     // console.log(source_href,source_src,script_src)
     if(source_href){
         for(var i=0;i<source_href.length;i++){
@@ -67,8 +67,9 @@
             tmp_href = href.substring(0,href.indexOf('#'))
             return tmp_href.substring(0,tmp_href.lastIndexOf('/')+1)+u;
         }else{
-            console.log(u)
-            return ;
+            // console.log("not match prefix:"+u+",like http // / ./")
+            tmp_href = href.substring(0,href.indexOf('#'))
+            return tmp_href.substring(0,tmp_href.lastIndexOf('/')+1)+u;
         }
     }
     
@@ -80,20 +81,7 @@
                 dataType: 'text',
             })
         .done(function(data) {
-                var search_data = {'current':href}
-                search_data['sfz'] = data.match(/['"]((\d{8}(0\d|10|11|12)([0-2]\d|30|31)\d{3}$)|(\d{6}(18|19|20)\d{2}(0[1-9]|10|11|12)([0-2]\d|30|31)\d{3}(\d|X|x)))['"]/g);
-                search_data['mobile'] = data.match(/['"](1(3([0-35-9]\d|4[1-8])|4[14-9]\d|5([\d]\d|7[1-79])|66\d|7[2-35-8]\d|8\d{2}|9[89]\d)\d{7})['"]/g);
-                search_data['mail'] = data.match(/['"][a-zA-Z0-9\._\-]*@[a-zA-Z0-9\._\-]{1,63}\.((?!js|css|jpg|jpeg|png|ico)[a-zA-Z]{2,})['"]/g);
-                search_data['ip'] = data.match(/['"]\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}['"]/g);
-                search_data['ip_port'] = data.match(/['"]\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\:\d{1,3}['"]/g);
-                search_data['domain'] = data.match(/['"][a-zA-Z0-9\-\.]*?\.(xin|com|cn|net|com.cn|vip|top|cc|shop|club|wang|xyz|luxe|site|news|pub|fun|online|win|red|loan|ren|mom|net.cn|org|link|biz|bid|help|tech|date|mobi|so|me|tv|co|vc|pw|video|party|pics|website|store|ltd|ink|trade|live|wiki|space|gift|lol|work|band|info|click|photo|market|tel|social|press|game|kim|org.cn|games|pro|men|love|studio|rocks|asia|group|science|design|software|engineer|lawyer|fit|beer|我爱你|中国|公司|网络|在线|网址|网店|集团|中文网)['"]/g);
-                search_data['path'] = data.match(/['"](?:\/|\.\.\/|\.\/)[^\/\>\< \)\(\{\}\,\'\"\\]([^\>\< \)\(\{\}\,\'\"\\])*?['"]/g);
-                search_data['url'] = data.match(/['"](([a-zA-Z0-9]+:)?\/\/)?[a-zA-Z0-9\-\.]*?\.(xin|com|cn|net|com.cn|vip|top|cc|shop|club|wang|xyz|luxe|site|news|pub|fun|online|win|red|loan|ren|mom|net.cn|org|link|biz|bid|help|tech|date|mobi|so|me|tv|co|vc|pw|video|party|pics|website|store|ltd|ink|trade|live|wiki|space|gift|lol|work|band|info|click|photo|market|tel|social|press|game|kim|org.cn|games|pro|men|love|studio|rocks|asia|group|science|design|software|engineer|lawyer|fit|beer|我爱你|中国|公司|网络|在线|网址|网店|集团|中文网)(\:\d{1,5})?(\/.*?)?['"]/g);
-                search_data['jwt'] = data.match(/['"'](ey[A-Za-z0-9_-]{10,}\.[A-Za-z0-9._-]{10,}|ey[A-Za-z0-9_\/+-]{10,}\.[A-Za-z0-9._\/+-]{10,})['"']/g);
-                // search_data['algorithm'] = data.match(/\WBase64\.encode\(|\WBase64\.decode\(|\Wbtoa\(|\Watob\(|\WCryptoJS\.AES\.|\WCryptoJS\.DES\.|\WJSEncrypt\(|\Wrsa\.|\WKJUR\.|\W$\.md5\(|\Wmd5\(|\Wsha1\(|\Wsha256\(|\Wsha512\(/gi);
-                search_data['algorithm'] = data.match(/\W(Base64\.encode|Base64\.decode|btoa|atob|CryptoJS\.AES|CryptoJS\.DES|JSEncrypt|rsa|KJUR|$\.md5|md5|sha1|sha256|sha512)[\(\.]/gi);
-                chrome.extension.sendMessage({greeting: "result",data: search_data}, function(response) { }  );
-                // console.log(search_data)
+                chrome.extension.sendMessage({greeting: "result",data: data, current: href}, function(response) { }  );
             })
         return ;
     }
--- popup.js
+++ popup.js
@@ -1,10 +1,14 @@
 // @Date    : 2020-09-12 16:26:48
 // @Author  : residuallaugh
 const bg = chrome.extension.getBackgroundPage();
-var key = ["ip","ip_port","domain","path","url","static","sfz","mobile","mail","jwt","algorithm"]
+var key = ["ip","ip_port","domain","path","incomplete_path","url","static","sfz","mobile","mail","jwt","algorithm","secret"]
 chrome.tabs.getSelected(null, function (tab) {
         var current=tab.url;
         var result_data = bg.result(current);
+        if(result_data == undefined){
+            console.log('还未提取完成');
+            return;
+        }
         for (var k in key){
             if (result_data[key[k]]!=null && result_data[key[k]].length != 0){
                 // console.log(result_data[key[k]])
@@ -17,3 +21,16 @@ chrome.tabs.getSelected(null, function (tab) {
             }
         }
 });
+
+$(function () {
+    $('.copy').click(function () {
+        console.log('copy begin');
+        var inp =document.createElement('textarea');
+        document.body.appendChild(inp)
+        inp.value =$($(this).attr('name')).text();
+        inp.select();
+        document.execCommand('copy',false);
+        inp.remove();
+        console.log('copy end');
+    })
+})
