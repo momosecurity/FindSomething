e2d39cfff7f59690e7fefd18a21ab000b4e8a1f2
liu.ruxue <liu.ruxue@hellogroup.com>
2023-02-16 23:52:11 +0800

    ver:2.0.12

--- popup.js
+++ popup.js
@@ -4,44 +4,46 @@ var key = ["ip","ip_port","domain","path","incomplete_path","url","static","sfz"
 
 function init_copy() {
     var elements = document.getElementsByClassName("copy");
-    for (var i=0, len=elements.length|0; i<len; i=i+1|0) {
-        let ele_name = elements[i].name;
-        let ele_id = elements[i].id;
-        elements[i].onclick=function () {
-            var inp =document.createElement('textarea');
-            document.body.appendChild(inp)
-            var copytext = document.getElementById(ele_name).textContent;
-            if (ele_id == "path_url_button"){
-                Promise.all([getCurrentTab().then(function x(tab){
-                    // console.log(tab);
-                    if(tab == undefined){
-                        alert("请点击原页面后再复制：）")
-                        return;
-                    }
-                    var url = new URL(tab.url)
-                    var path_list = copytext.split('\n')
-                    copytext = ""
-                    for (var i = path_list.length - 1; i >= 0; i--) {
-                        if(path_list[i][0] == '.'){
-                            copytext += url.origin+'/'+path_list[i]+'\n';
-                        }else{
-                            copytext += url.origin+path_list[i]+'\n';
+    if(elements){
+        for (var i=0, len=elements.length|0; i<len; i=i+1|0) {
+            let ele_name = elements[i].name;
+            let ele_id = elements[i].id;
+            elements[i].onclick=function () {
+                var inp =document.createElement('textarea');
+                document.body.appendChild(inp)
+                var copytext = document.getElementById(ele_name).textContent;
+                if (ele_id == "path_url_button"){
+                    Promise.all([getCurrentTab().then(function x(tab){
+                        // console.log(tab);
+                        if(tab == undefined){
+                            alert("请点击原页面后再复制：）")
+                            return;
                         }
-                    }
-                    inp.value = copytext.slice(0, -1);
-                    inp.select();
-                    // console.log(copytext)
-                    document.execCommand('copy',false);
-                    // inp.remove();
-                })]).then(res=> inp.remove())
+                        var url = new URL(tab.url)
+                        var path_list = copytext.split('\n')
+                        copytext = ""
+                        for (var i = path_list.length - 1; i >= 0; i--) {
+                            if(path_list[i][0] == '.'){
+                                copytext += url.origin+'/'+path_list[i]+'\n';
+                            }else{
+                                copytext += url.origin+path_list[i]+'\n';
+                            }
+                        }
+                        inp.value = copytext.slice(0, -1);
+                        inp.select();
+                        // console.log(copytext)
+                        document.execCommand('copy',false);
+                        // inp.remove();
+                    })]).then(res=> inp.remove())
+                    // alert('复制成功');
+                    return ;
+                }
+                inp.value = copytext;
+                inp.select();
+                document.execCommand('copy',false);
+                inp.remove();
                 // alert('复制成功');
-                return ;
             }
-            inp.value = copytext;
-            inp.select();
-            document.execCommand('copy',false);
-            inp.remove();
-            // alert('复制成功');
         }
     }
 };
@@ -58,7 +60,7 @@ function sleep (time) {
 
 function show_info(result_data) {
     for (var k in key){
-        if (result_data[key[k]]!=null && result_data[key[k]].length != 0){
+        if (result_data[key[k]]){
             // console.log(result_data[key[k]])
             let p="";
             for(var i in result_data[key[k]]){
@@ -82,7 +84,7 @@ getCurrentTab().then(function get_info(tab) {
         // console.log(result_data)
         if(result_data == undefined || result_data['done']!='done'){
             // console.log('还未提取完成');
-            if(result_data != undefined && result_data.donetasklist != undefined ){
+            if(result_data && result_data.donetasklist){
                 // console.log("findsomething_result_"+tab.url)
                 chrome.storage.local.get(["findsomething_result_"+tab.url], show_info(result_data));
                 // show_info(result_data);
